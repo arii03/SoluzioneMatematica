4c61c7c18cf5edf597bbedb2d1d9514aab34dcf1
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-04 13:20:10 +0200

    Add project files.

---
+++ LibreriaMatematica/Operazioni.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace LibreriaMatematica
+{
+    public class Operazioni
+    {
+        public static int Somma(int a, int b)
+        {
+            int s = a + b;
+            return s;
+        }
+        public static int Sottrazione(int n1, int n2)
+        {
+            int d = n1 - n2;
+            return d;
+        }
+        public static double Divisione(double n1, double n2)
+        {
+            if (n2 == 0)
+            {
+
+
+                throw new Exception("Divisione con zero non valida");
+            }
+            double q = n1 / n2;
+            return q;
+        }
+
+        public static double Max(double n1, double n2)
+        {
+            double ma;
+            if (n1 >= n2)
+            {
+                ma = n1;
+            } else
+            {
+                ma = n2;
+
+            }
+            return ma;
+        }
+        public static double Min(double n1, double n2)
+        {
+            double mi;
+            if (n1 <= n2)
+            {
+                mi = n1;
+            }
+            else
+            {
+                mi = n2;
+
+            }
+            return mi;
+        }
+        public static double Potenza(double n1, double n2)
+        {
+            int contatore = 0;
+            do
+            {
+                n1 = n1 * n1;
+                contatore++;
+            } while (contatore < n2);
+            return n1;
+        }
+
+    }
+}
---
+++ Matematica/Program.cs
@@ -0,0 +1,80 @@
+﻿using LibreriaMatematica;
+using System;
+
+namespace Matematica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inserisci il primo numero: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("Inserisci il secondo numero: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quale operazione vuoi fare? (A/S/D/P/Max/Min)");
+            string risposta = Console.ReadLine();
+
+            double risultato;
+            switch (risposta)
+            {
+                case "Max":
+                    risultato = Operazioni.Max(n1, n2);
+                    break;
+                case "Min":
+                    risultato = Operazioni.Min(n1, n2);
+                    break;
+                case "A":
+                    risultato = Operazioni.Somma(n1, n2);
+                    break;
+                case "S":
+                    risultato = Operazioni.Sottrazione(n1, n2);
+                    break;
+                case "D":
+                    risultato = Operazioni.Divisione(n1, n2);
+                    break;
+                case "P":
+                    risultato = Operazioni.Potenza(n1, n2);
+                    break;
+                default:
+                    throw new Exception("Scelta non valida");
+                    Console.WriteLine(risultato);
+                    try
+                    {
+                        risultato = Operazioni.Divisione(n1, n2);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        risultato = double.NaN;
+                    }
+            }
+
+
+            Console.WriteLine(risultato);
+            Console.ReadLine();
+
+            int somma = Operazioni.Somma(n1, n2);
+            Console.WriteLine(somma);
+
+            int sottrazione = Operazioni.Sottrazione(n1, n2);
+            Console.WriteLine(sottrazione);
+
+            double divisione = Operazioni.Divisione(n1, n2);
+            Console.WriteLine(divisione);
+            Console.ReadLine();
+
+            double potenza = Operazioni.Potenza(n1, n2);
+            Console.WriteLine(potenza);
+            Console.ReadLine();
+
+            double max = Operazioni.Max(n1, n2);
+            Console.WriteLine(max);
+            Console.ReadLine();
+
+            double min = Operazioni.Min(n1, n2);
+            Console.WriteLine(min);
+            Console.ReadLine();
+        }
+    }
+}
